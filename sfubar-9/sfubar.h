6992e448b9b7c304bead6d3e946c1b9b6db3de3f
звездочёт <mykaralw@yandex.ru>
2017-12-06 20:18:05 +0300

    0.20161001

--- sfubar-9/sfubar.h
+++ sfubar-9/sfubar.h
@@ -1,7 +1,7 @@
 #ifndef __SFUBAR_H__
 #define __SFUBAR_H__
-#include <trap.h>
-#include <aifutil.h>
-#include <sfutil.h>
-#include <wavutil.h>
+#include "trap.h"
+#include "aifutil.h"
+#include "sfutil.h"
+#include "wavutil.h"
 #endif
